fcbba6d9f498573fed06308f1ef3756c4858aac6
Eric S. Londres <elondres@stevens.edu>
2019-08-04 01:13:47 -0400

    Expose QProcess module to QML

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "process.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,9 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
+
+    qmlRegisterType<Process>("Process", 1, 0, "Process");
+
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
