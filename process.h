c48f46d0d7ed8599a19c02bc6b524be62948d6d1
Eric S. Londres <elondres@stevens.edu>
2019-08-04 03:25:21 -0400

    Formatting

--- process.h
+++ process.h
@@ -1,18 +1,18 @@
 /* TextConnect
-    Copyright (C) 2019  Eric S. Londres
+   Copyright (C) 2019  Eric S. Londres
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef PROCESS_H
@@ -22,24 +22,24 @@
 #include <QVariant>
 
 class Process : public QProcess {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    Process(QObject * parent = nullptr) : QProcess(parent) { }
+  Process(QObject * parent = nullptr) : QProcess(parent) { }
 
-    Q_INVOKABLE void start(const QString &program, const QVariantList &arguments) {
-        QStringList args;
-        // convert QVariantList from QML to QStringList for QProcess
-        for(int i = 0; i < arguments.length(); ++i) {
-            args << arguments[i].toString();
-        }
-
-        QProcess::start(program, args);
+  Q_INVOKABLE void start(const QString &program, const QVariantList &arguments) {
+    QStringList args;
+    // convert QVariantList from QML to QStringList for QProcess
+    for(int i = 0; i < arguments.length(); ++i) {
+      args << arguments[i].toString();
     }
 
-    Q_INVOKABLE QByteArray readAll() {
-        return QProcess::readAll();
-    }
+    QProcess::start(program, args);
+  }
+
+  Q_INVOKABLE QByteArray readAll() {
+    return QProcess::readAll();
+  }
 };
 
 #endif // PROCESS_H
